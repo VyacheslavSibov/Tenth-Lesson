3d1b9876f8e3a8b8132745060a570496a2b333f8
VyacheslavSibov <105800498+VyacheslavSibov@users.noreply.github.com>
2022-07-19 21:28:32 +0300

    Update Pizza.java

--- src/ua/ithillel/java/Pizza.java
+++ src/ua/ithillel/java/Pizza.java
@@ -9,7 +9,7 @@ public class Pizza {
     double cost;
 
     public Pizza(double diameter, String[] composition, String name, double cost) {
-        double radius = diameter / 3;
+        double radius = diameter;
         basis = new Circle(radius);
         this.composition = composition;
         this.name = name;
